e3e299e8628c3c06b003cdf3bb7aad31c9e3dd45
Fernando <fzavala649@gmail.com>
2018-02-15 16:24:28 -0800

    Added keyboard checking
    Changed over to classes so we can split up everything, added some keyboard tracking so know we know if the user hit the arrow keys, this will be the base for mimicking menu interactivity, my idea is to have the first option be the highlighted one and with every key press we change the highlighted one to the next one so if they hit down it goes down.

--- project1/source.py
+++ project1/source.py
@@ -2,35 +2,71 @@ import os, sys
 import  pygame
 from pygame.locals import *
 bg = (255, 255, 255)
-displayHeight = 800
-displayWidth  = 600
+res = [1200, 720]
+
+pygame.init()
+pygame.font.init()
+
+
+class main_menu(object):
+    def __init__(self, resolution, title_size, options_size, background, title_text, title_color, c_color,  c1_text ,c2_text, c3_text):
+        self.screen = pygame.display.set_mode(resolution, 0)
+        self.resolution = resolution
+        self.menu_title = pygame.font.SysFont('Times New Roman', title_size)
+        self.menu_button = pygame.font.SysFont('Times New Roman', options_size)
+        self.background_color = background
+        self.title = self.menu_title.render(title_text, True, title_color)
+        self.choice1 = self.menu_button.render(c1_text, True, c_color)
+        self.choice2 = self.menu_button.render(c2_text, True, c_color)
+        self.choice3 = self.menu_button.render(c3_text, True, c_color)
+        self.screen.fill(self.background_color)
+        self.menu_choice_tracker = 1
+
+    def drawButtonRect(self):
+        pass
+    def displayMenu(self):
+        self.screen.blit(self.title, ( self.resolution[0] / 2 - 55,  self.resolution[1] / 2 - 200))
+        self.screen.blit(self.choice1, ( self.resolution[0] / 2 - 30,  self.resolution[1] / 2 - 75))
+        self.screen.blit(self.choice2, ( self.resolution[0] / 2 - 30,  self.resolution[1] / 2 - 35))
+        self.screen.blit(self.choice3, ( self.resolution[0] / 2 - 30,  self.resolution[1] / 2 + 5))
+        pygame.display.update()
+
+    def setMenuChoice(self):
+        pass
+
+class AdvGame(object):
+    def __init__(self, obj):
+        self.menuObj = obj
+
+    def startGame(self):
+        self.menuObj.displayMenu()
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                if event.type == pygame.KEYDOWN:
+                    self.checkKeyboardInput(event)
+
+            pygame.display.update()
+
+    def checkKeyboardInput(self, event): # change this to reflect user choice
+        if event.key == pygame.K_LEFT:
+            print("left")
+        if event.key == pygame.K_RIGHT:
+            print("right")
+        if event.key == pygame.K_DOWN:
+            print("down")
+        if event.key == pygame.K_UP:
+            print("up")
+            # also add the enter key option!!!
+
+
 def main():
-    pygame.font.init()
-    pygame.init()
-    screen = pygame.display.set_mode([displayHeight, displayWidth], 0)
-    menu_title = pygame.font.SysFont('Times New Roman', 30)
-    menu_button = pygame.font.SysFont('Times New Roman', 15)
-    screen.fill(bg)
-    pygame.display.set_caption("CYOA")
-
-    title   = menu_title.render("#MENU#", True, (0, 0, 0))
-    choice1 = menu_button.render("#CHOICE#", True, (0, 0, 0))
-    choice2 = menu_button.render("#CHOICE#", True, (0, 0, 0))
-    choice3 = menu_button.render("#CHOICE#", True, (0, 0, 0))
-    screen.blit(title, (displayHeight/2  - 55, displayWidth/2 - 200))
-    screen.blit(choice1, (displayHeight/2 -30, displayWidth/2 -75))
-    screen.blit(choice2, (displayHeight / 2 - 30, displayWidth / 2 - 35))
-    screen.blit(choice3, (displayHeight / 2 - 30, displayWidth / 2 +5))
-
-    pygame.display.update()
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
+    menu = main_menu(res, 50, 35, bg, "MENU", (0, 0, 0), (0, 0, 0), "C1", "C2", "C3")
+    #menu.displayMenu()
+    game = AdvGame(menu)
+    game.startGame()
 
 if __name__ == '__main__':
-   main()
-
+    main()
